2b517a2854c30090b7d26b763e75283896dda657
Giovanna Castro <gicacv@hotmail.com>
2017-05-10 14:10:37 -0300

    oioioi

---
+++ search_issues.php
@@ -0,0 +1,18 @@
+<? php
+
+  include(mapa.html);
+
+      $search_input = $POST['busca'];
+
+
+      use UnBix_database;
+      foreach ($pdo->query($sql) as $row)
+      {
+
+
+
+      }
+      Database::disconnect();
+
+
+ ?>
