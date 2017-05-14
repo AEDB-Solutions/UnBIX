eec3f510c45ea1b8ab0bf1c2f4fe0d3c7b3b4bf2
Luiz <luiz.pires11@gmail.com>
2017-05-14 11:43:05 -0300

    acrescentei duas prorpiedadades na classe

--- class_database_query.php
+++ class_database_query.php
@@ -8,17 +8,16 @@ class db_query
 	private $db_rows;//array_bidimensional
 
 
-	public function set_find_row($colluns_to_search,$db_table,$after_where,$search_elements)//query que busque exatamente uma coluna;objeto achar uma linha do banco;
+	public function set_find_row($query,$search_elements)//query que busque exatamente uma coluna;objetivo achar uma linha do banco;
 	{
 		$db = Database::connect();	
 		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$query = "select $colluns_to_search from  $db_table WHERE $after_where;";
 		$prepare = $db->prepare($query, array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
 		$prepare->execute($search_elements);
 
 		  while ($row = $prepare->fetch(PDO::FETCH_BOTH, PDO::FETCH_ORI_NEXT))
 		  {
-		  		$this->db_element = $row;
+		  		$this->db_row = $row;
 		  		//break;
 		  }
 
@@ -28,13 +27,13 @@ class db_query
 
 	public function get_row()
 	{
-		return $this->db_element;
+		return $this->db_row;
 	}
 
-//--------------------------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------------------
 
 
-	public function set_find_element($query,$search_elements,$db_collun)//coloque um query que busque no maximo uma coluna;objeto é achar um elemento
+	public function set_find_element($query,$search_elements,$db_collun)//coloque um query que busque no maximo uma coluna;objetivo é achar um elemento
 	{
 		$db = Database::connect();	
 		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -43,10 +42,12 @@ class db_query
 
 		while($row = $prepare->fetch(PDO::FETCH_BOTH, PDO::FETCH_ORI_NEXT))
 		{
-			$sigle_element = $row[$db_collun];
+			$single_element = $row[$db_collun];
 		}
 
-		return $single_element;
+		Database::disconnect();
+
+		$this->db_element = $single_element;
 
 	}
 
@@ -56,11 +57,20 @@ class db_query
 	}
 
 
-//---------------------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------------------
 
-	public function set_find_rows()//query que busca um varias linhas do banco de dados;array multi
+	public function set_find_rows($query,$search_elements)//query que busca um varias linhas.Qualquer query serve//o programador pode buscar varias linhas e obter um elemento por manipulação propria;
 	{
-		//...
+		$db = Database::connect();	
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$prepare = $db->prepare($query);
+		$prepare->execute($search_elements);
+		$result = $prepare->fetchAll();
+		Database::disconnect();
+
+		$this->db_rows = $result;
+
+
 	}
 
 	public function get_db_rows()
