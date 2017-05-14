22dd635236524650859b656376197aedf1daf3d0
Luiz <luiz.pires11@gmail.com>
2017-05-14 00:10:56 -0300

    restruturei melhor os arquivos validating_login_info e validating_register_info.php incluindo função que carrega informações do usuario durante toda a sessão

--- validating_login_info.php
+++ validating_login_info.php
@@ -3,7 +3,23 @@
 header("Content-Type: text/html; charset=ISO-8859-1", true);
 
 include("database.php");
+include("class_database_query.php");
 
+function login_check_info()
+{
+
+	$erros = array('matriculaErr' => ' ','passErr' => ' ', 'generalErr' => ' ');
+
+	analysing_post_empty('matricula','Matricula', $erros);
+	analysing_post_empty('pass','Senha', $erros);
+
+	$password = $_POST['pass'];
+	$mat = $_POST['matricula'];
+
+	$db_colluns = array("Userid","Curso","Nome","Matricula","Email" ,"Senha", "Nascimento","Genero");
+	
+	try_login($mat, $password, $db_colluns, $erros);
+}
 
 function analysing_post_empty($type, $db_collun, &$erros)
 {
@@ -16,26 +32,77 @@ function analysing_post_empty($type, $db_collun, &$erros)
 	}
 }
 
-function validate_all_info($matricula, $senha, $db_colluns)
+function try_login($matricula , $pass, $db_colluns, $erros)
+{
+	if(validate_login_info($matricula, $pass, $db_colluns))
+	{
+		setting_basic_info($matricula, $pass);
+		echo '<html><head><meta http-equiv="Refresh" content="1;mapa.php"></head></html>';
+		exit;
+	}
+
+	else if($erros['passErr'] == ' ' && $erros['matriculaErr'] == ' ')
+	$erros['generalErr'] = 'Matricula ou senha Inválida!';
+
+//imprimindo erros depois daqui
+	printing_erros($erros);
+}
+
+function printing_erros($erros)
+{
+	foreach ($erros as $term) 
+	{
+		if($term != ' ')
+		{
+				echo "<script>alert('$term');</script>";
+				echo '<html><head><meta http-equiv="Refresh" content="1;tela_login.php"></head></html>';
+				break;
+		}	
+		//header()
+	}
+}
+
+function validate_login_info($matricula, $senha, $db_colluns)
 {
 	//$pdo = new Database;//objet database//nao funciona?
 	$db = Database::connect();	
 
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	$cript_senha= hash('sha256',$senha);
+	
 
-	$query = "Select * from Users where {$db_colluns[3]} = '{$matricula}' and {$db_colluns[5]} = '{$cript_senha}';";
+	$cript_senha= hash('sha256',$senha);
+	$query = "Select * from Users where {$db_colluns[3]} = ?  and {$db_colluns[5]} = ?;";
 	$finds = $db->prepare($query);
-	$finds->execute();
+	$finds->execute(array($matricula, $cript_senha));
 	$rows = $finds->rowCount();
 
 	if($rows > 0)
 	{
-		Database::disconnect();
-			return true;
+		return true;
 	}
-		Database::disconnect();
-		return false;
+
+	return false;	
+}
+
+function setting_basic_info($matricula, $senha)
+{
+	$row = picking_user_datas($matricula, $senha);
+
+	$_SESSION['id'] = $row['Userid'];
+	$_SESSION['name'] = $row['Nome'];
+	$_SESSION['matricula'] = $row['Matricula'];
+	$_SESSION['email'] = $row['Email'];
+
+	
+}
+
+function picking_user_datas($matricula, $senha)
+{
+	$find_element = new db_query();
+	$find_element->set_find_row("Matricula,Email,Nome,Userid","Users","Matricula = ? and Senha = ?", array($matricula, hash('sha256',$senha)));
+	$row = $find_element->get_row();
+	
+	return $row;
 }
 
 ?>
--- validating_register_info.php
+++ validating_register_info.php
@@ -4,6 +4,59 @@ header("Content-Type: text/html; charset=ISO-8859-1", true);
 
 	include("database.php");
 
+	function registering()
+	{
+
+    	$erros = array('nomeErr' => ' ', 'emailErr' => ' ', 'matriculaErr' => ' ','senhaErr' => ' ', 'confsenhaErr' => ' ', 'cursoErr' => ' ', 'generoErr' => ' ');
+    	$posts_names= array('nome','email','matricula','senha', 'confsenha', 'curso','genero');
+
+    	for($i = 0; $i < count($posts_names); $i++)
+    	validating_posts($posts_names[$i], $erros);
+
+    	if(no_erro($erros))
+       		pass_to_db();
+
+    else
+        print_register_erros($erros);
+	}
+
+	function pass_to_db()
+	{
+        $nome = $_POST['nome'];
+        $curso = $_POST['curso'];
+        $email = $_POST['email'];
+        $matricula = $_POST['matricula'];
+        $genero = $_POST['genero'];
+        $senha_in = $_POST['senha'];
+        $senha_arm = hash('sha256',$senha_in);
+
+            $pdo = Database::connect();
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sql = "INSERT INTO Users (Nome,Curso,Email,Matricula,Genero,Senha) values(?, ?, ?, ?, ?, ?);";
+            $q = $pdo->prepare($sql);
+            $q->execute(array($nome,$curso,$email,$matricula,$genero,$senha_arm));
+            Database::disconnect();
+
+        echo '<html><p style=" text-align:center;">Registro feito com sucesso!</p><a href="index.php"> Voltar << </a> </html>';
+
+	}
+
+	function print_register_erros($erros)
+	{
+        $emailErr = $erros['emailErr'];
+        $matriculaErr = $erros['matriculaErr'];
+            
+        if($erros['emailErr'] != ' ')
+        echo "<script>alert('$emailErr');</script>";
+
+        else if($erros['matriculaErr'] != ' ')
+        echo "<script>alert('$matriculaErr');</script>";
+
+        echo '<html><head><meta http-equiv="Refresh" content="1;cadastro.php"></head></html';
+	}	
+
+
+
 	function validating_posts($post_name,&$erros)
 	{
 			$key = $post_name."Err";
@@ -81,3 +134,4 @@ header("Content-Type: text/html; charset=ISO-8859-1", true);
 	}
 
  ?>
+
