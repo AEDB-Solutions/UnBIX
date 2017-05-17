e4601a733745630747a5eaf2022a7a8b205c2d7c
Larissa Pires <larissa.pires98@gmail.com>
2017-05-17 15:24:44 -0300

    ATUALIZAÇÃO PROVISÓRIA

--- savelocation.php
+++ savelocation.php
@@ -9,18 +9,19 @@
         $lat = $_POST['lat'];
         $long = $_POST['long'];
         $reclam = $_POST['reclam'];
+        $adicional = $_POST['adic'];
         $categ = $_POST['categ'];
         $titulo = $_POST['titulo'];
 
 
 
+
             $pdo = Database::connect();
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $sql = "INSERT INTO Complaints (Latitude, Longitude, Descrição, Categoria, Título) values(?, ?, ?, ?, ?);";
+            $sql = "INSERT INTO Complaints (IDuser, Titulo, Descriçao, Descriçao_adicional, Latitude, Longitude, Categoria) values (1, $titulo, $reclam, $adicional, $lat, $long, $categ);"; //O IDuser é temporario para testes enquanto nao temos sessão. 
             $q = $pdo->prepare($sql);
             $q->execute(array($lat, $long, $reclam, $categ, $titulo));
             Database::disconnect();
         }
     
 ?>
-
