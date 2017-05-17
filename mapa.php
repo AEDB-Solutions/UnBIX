a68096267646c740e979d83c3349736d921aa13b
Larissa Pires <larissa.pires98@gmail.com>
2017-05-17 16:27:10 -0300

    FUNÇÃO MELHORZINHA

--- mapa.php
+++ mapa.php
@@ -9,7 +9,8 @@
   </head>
 
   <body>
-  <button onclick="getLocation()">Inserir reclamação na localização atual</button>
+<p><button onclick="geoFindMe()">Inserir reclamação na localização atual</button></p>
+<div id="out"></div>
 
   <p id="demo"></p>
 
@@ -17,18 +18,18 @@
       <tr><td></td> <td><input type="hidden" name="lat" id="lat" /> </td> </tr>
       <tr><td></td> <td><input type="hidden" name="long" id="long" /> </td> </tr>
       <tr><td>Título:</td> <td><input type="text" name="titulo" /> </td> </tr>
-     <tr><td>Reclamação:</td> <td><input type="text" name="reclam" /> </td> </tr>
+      <tr><td>Reclamação:</td> <td><input type="text" name="reclam" /> </td> </tr>
       <tr><td>Categoria:</td> <td><select name="categ"> +
       <option value="Infraestrutura" SELECTED>Infraestrutura</option>
       <option value="Mau-Funcionamento">Mau-Funcionamento</option>
       <option value="Outro">Outro</option>
       </select> </td></tr>
-     <tr><td></td><td><input type='submit' value='Reclame!'/></td></tr>
-   </form>
+      <tr><td></td><td><input type='submit' value='Reclame!'/></td></tr>
+    </form>
 
         <div id="map-canvas"></div>
         <script src="script.js"></script>
-       <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB1WMWZn7OQEGUH0lCnd-3i9krdCkA8LoY&callback=initMap" type="text/javascript"></script>
+        <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB1WMWZn7OQEGUH0lCnd-3i9krdCkA8LoY&callback=initMap" type="text/javascript"></script>
   </body>
 
 </html>
