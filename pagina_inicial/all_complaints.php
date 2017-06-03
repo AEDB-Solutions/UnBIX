1f9e3b4ab73dfc4959e73a32f1e87911c7e69678
Luiz <luiz.pires11@gmail.com>
2017-06-03 17:44:55 -0300

    pegando todas as localizacoes do banco de dados e carregando no mapa

---
+++ pagina_inicial/all_complaints.php
@@ -0,0 +1,7 @@
+<?php
+
+include("functions_for_json_info.php");
+
+echo json_encode(picking_complaints());
+
+?>
--- pagina_inicial/functions_for_json_info.php
+++ pagina_inicial/functions_for_json_info.php
@@ -6,7 +6,16 @@ include("class_database_query.php");
 function picking_key_points()
 {
 	$find_rows = new db_query();
-	$find_rows->set_find_rows("SELECT * FROM Localidades;",array(""));
+	$find_rows->set_find_rows("SELECT * FROM Localidades where keypoint = 1;",array(""));
+	$rows = $find_rows->get_db_rows();
+
+	return $rows;
+}
+
+function picking_complaints()
+{
+	$find_rows = new db_query();
+	$find_rows->set_find_rows("SELECT * FROM Localidades where keypoint = 0;",array(""));
 	$rows = $find_rows->get_db_rows();
 
 	return $rows;
