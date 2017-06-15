5f6613388f338417d03747b19884829b7ed514c5
Larissa Pires <larissa.pires98@gmail.com>
2017-06-14 21:53:32 -0300

    Busca por categoria pronta, fornece um array multidimensional completo

--- pagina_inicial/buscacateg.php
+++ pagina_inicial/buscacateg.php
@@ -1,72 +1,33 @@
 <?php
 
 include("database.php");
-//var_dump(get_categ()); 
 
-function get_categ() // TESTADA E FUNCIONANDO -- função que pega a categoria e busca no banco todas as reclamações daquela categoria
-{			
-		if (!empty($_GET)) 
+//var_dump(get_reclam());
+
+function get_reclam() // TESTADA E FUNCIONANDO -- função que pega a categoria e busca no banco todas as reclamações daquela categoria e junta em um array multidimensional todas as informações, inclusive as que estao no localidaddes;
+{
+        if (!empty($_GET)) 
         { 
 
-         
             //$id_user = $_SESSION['id']; - pensar caso a reclamação seja dele ou não 
             $categ = $_GET['categ_'];
-			
-			$pdo = Database::connect();
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
-            $sql = "SELECT * FROM Complaints WHERE Categoria = ?;";
-            $q = $pdo->prepare($sql);
-            $q->execute(array($categ));
-            $result = $q->fetchAll();
-            //var_dump($q); exit;
-            Database::disconnect();
 
-            return $result; //retorna TODAS as informações de TODAS AS RECLAMAÇÕES DAQUELA CATEGORIA
-        }
-        else 
-        { 
-       	return 0;  
-        }
 
-}
-
-function get_coords() //TESTADA E FUNCIONANDO -- função que pega posição a posição do get_categ e procura seu respectivo na localidades, e armazena na mesma posição em outro array
-{
-   		$compl = get_categ();
-        $localid = array_column($compl,'LocalID'); 
-        //var_dump($localid); exit;
-        $size = count($localid);
-        //var_dump($size); exit; 
-        $all = NULL;
-
-        for($i = 0; $i < $size; $i++)
-        {
-            $one = $localid[$i]; 
             $pdo = Database::connect();
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
-            $sql = "SELECT * FROM Localidades WHERE localID = $one;";
+            $sql = "SELECT Complaints.LocalID, Complaints.Titulo, Complaints.Descricao, Complaints.Categoria, Complaints.Emergencia, Localidades.descricao, Localidades.latitude, Localidades.longitude FROM Complaints INNER JOIN Localidades ON Complaints.Categoria = ? AND Complaints.LocalID=Localidades.localID;";
             $q = $pdo->prepare($sql);
-            $q->execute();
-            //var_dump(array($one)); exit;
+            $q->execute(array($categ));  
             $result = $q->fetchAll();
-            //var_dump($result); exit;
-            $all[$i] = $result; 
-            //var_dump($q); exit;
-            Database::disconnect();       
+            //var_dump($result);exit;
+            Database::disconnect();   
+
+            return $result;  
+        }      
+        else
+        {
+            return 0;
         }
-
-        return $all; // retorna as informações de LOCALIZAÇÃO de TODOS AS RECLAMAÇÕES 
-
-}
-
-function info_marker() 
-{
-			$reclm = get_categ();
-			$coords = get_coords(); 
-
-           // var_dump($reclm); 
-           // var_dump($coords);
-        // COLOCAR AQUI O QUE DEVE SER RETORNADO PARA O JAVASCRIPT GERAR MARKER
 }
 
 ?>
