6e22338ece08d974089157248a73c5bc57a24f41
Luiz <luiz.pires11@gmail.com>
2017-06-15 15:44:13 -0300

    functions for json

--- pagina_inicial/functions_for_json_info.php
+++ pagina_inicial/functions_for_json_info.php
@@ -39,7 +39,7 @@ function getting_form_info()
 	{
 	//var_dump($rows[$i]["localID"]);
 		$find_row = new db_query();
-	$find_row->set_find_row("SELECT ComplaintID,IDuser,Titulo,Categoria,Emergencia,Descricao FROM Complaints WHERE LocalID = ?;",array($rows[$i]["localID"]));
+		$find_row->set_find_row("SELECT ComplaintID,IDuser,Titulo,Categoria,Emergencia,Descricao FROM Complaints WHERE LocalID = ?;",array($rows[$i]["localID"]));
 		$row_complain = $find_row->get_row();
 
 
