88ce282615a801eb3c35f2beacae9536b3bfb74d
jorgerobertosf <jorginho.silveira@hotmail.com>
2017-06-22 23:55:28 -0300

    Update chart.php

--- pagina_inicial/chart.php
+++ pagina_inicial/chart.php
@@ -197,7 +197,7 @@ Obs: A posicao da barra de navegacao esta com style="position: absolute;" pois a
 
       </main>
 
-      <div id="piechart" style="width: 900px; height: 500px;"></div>
+      <div id="piechart" style="width: 1200px; height: 800px;"></div>
 
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
