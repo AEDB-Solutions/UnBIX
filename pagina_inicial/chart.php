b502b21e420b9939b0a71f3f93dc0a84cc9a92f9
jorgerobertosf <jorginho.silveira@hotmail.com>
2017-06-28 09:00:50 -0300

    Update chart.php

--- pagina_inicial/chart.php
+++ pagina_inicial/chart.php
@@ -29,6 +29,7 @@ if(empty($_SESSION['id'])) {
   <title>Relatorios</title>
   <meta charset="utf-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>	
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
   <link type="text/css" rel="stylesheet" href="css/materialize.min.css" />
