eed4eafbbcf18154ee19f1723d5fac8805fc2fde
Larissa Pires <larissa.pires98@gmail.com>
2017-07-03 11:15:57 -0300

    funciona

--- pagina_inicial/chamarcaminho.php
+++ pagina_inicial/chamarcaminho.php
@@ -3,13 +3,15 @@
 include("pathway.php"); 
 
             //$id_user = $_SESSION['id']; - pensar caso a reclamação seja dele ou não 
-//$categ = $_POST['B'];
-//$raio = $_POST['raio'];
+$categ = $_GET['categ'];
+$raio = $_GET['raio'];
 
-$categ = 'Banheiro F';
-$raio = 500;
+//$categ = 'Banheiro F';
+//$raio = 500;
 
 echo json_encode(which_categ($categ, $raio)); //nao reconhece pq é uma função js dentro do html 
+
+
 
 //e se pegaar a categ pelo post do botao, e fazer um php novo e colocar aqui e fzer a busca de novo e chama aqui ja igual o loc_0
 
